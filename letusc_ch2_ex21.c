91dcade5b5a94f70b5b81ae484d3fc390484f003
Arunavo Ghosh <arunavoghosh29@gmail.com>
2026-02-05 09:49:11 +0530

    Implement program to evaluate student marks and determine pass/fail status

--- letusc_ch2_ex21.c
+++ letusc_ch2_ex21.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 void main()
 {
-    
+    float a,b;
+    printf("Enter the marks obtained in the subject A:\n");
+    scanf("%f", &a);
+    printf("Enter the marks obtained in the subject B:\n");
+    scanf("%f", &b);
+    if(a>=55 && b>=45)
+    {
+        printf("Passed\n");
+    }
+    else if (a>=45 && a<55 && b>=55)
+    {
+        printf("Passed\n");
+    }
+    else if(a>=65 && b<45)
+    {
+        printf("You are allowed to reappear for B\n");
+    }
+    else
+    {
+        printf("Failed\n");
+    }
 }
