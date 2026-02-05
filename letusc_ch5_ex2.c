6f958a0c89d0e6433263e551585f90d96b05180c
Arunavo Ghosh <arunavoghosh29@gmail.com>
2026-02-05 19:00:48 +0530

    Implement program to calculate and display prime factors of a given integer

--- letusc_ch5_ex2.c
+++ letusc_ch5_ex2.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
-void main()
-{
-    
+
+void prime(int n, int i) {
+    if (n == 1) return;
+
+    if (n % i == 0) {
+        printf("%d ", i);
+        prime(n / i, i);
+    } else {
+        prime(n, i + 1);
+    }
+}
+
+void main() {
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    printf("Prime factors of %d are: ", num);
+    prime(num, 2);
+
+    printf("\n");
 }
